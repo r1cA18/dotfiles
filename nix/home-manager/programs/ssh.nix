828f17aa185dbd1650d34e705b1a4ceff20f585b
r1cA18 <r1cA18@proton.me>
2026-05-21 06:47:12 +0900

    feat: add ssh agent homelab

--- nix/home-manager/programs/ssh.nix
+++ nix/home-manager/programs/ssh.nix
@@ -7,7 +7,11 @@ lib.mkIf pkgs.stdenv.isDarwin {
       "*" = {
         extraOptions = {
           IdentityAgent = "\"~/Library/Group Containers/2BUA8C4S2C.com.1password/t/agent.sock\"";
+          IdentitiesOnly = "yes";
         };
+      };
+      "homelab" = {
+        user = "r1ca18";
       };
     };
   };
