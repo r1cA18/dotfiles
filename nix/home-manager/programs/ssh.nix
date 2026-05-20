88c66804b369c0b2ff7490409672869f4f7af16a
r1cA18 <r1cA18@proton.me>
2026-05-21 06:54:06 +0900

    chore: remove IdentitiesOnly

--- nix/home-manager/programs/ssh.nix
+++ nix/home-manager/programs/ssh.nix
@@ -7,7 +7,6 @@ lib.mkIf pkgs.stdenv.isDarwin {
       "*" = {
         extraOptions = {
           IdentityAgent = "\"~/Library/Group Containers/2BUA8C4S2C.com.1password/t/agent.sock\"";
-          IdentitiesOnly = "yes";
         };
       };
       "homelab" = {
