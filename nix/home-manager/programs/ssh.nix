e3ff622af77a8845369c7c33da77f87f8cef0358
r1cA18 <r1cA18@proton.me>
2026-05-21 07:22:50 +0900

    feat(ssh): export SSH_AUTH_SOCK to 1Password agent

--- nix/home-manager/programs/ssh.nix
+++ nix/home-manager/programs/ssh.nix
@@ -1,5 +1,9 @@
 { pkgs, lib, ... }:
 lib.mkIf pkgs.stdenv.isDarwin {
+  home.sessionVariables = {
+    SSH_AUTH_SOCK = "$HOME/Library/Group Containers/2BUA8C4S2C.com.1password/t/agent.sock";
+  };
+
   programs.ssh = {
     enable = true;
     enableDefaultConfig = false;
